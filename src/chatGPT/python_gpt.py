a7f7e74b173b7624720dee2cf3e94356e63680e1
jcrzz <juancruzdelvalle12@gmail.com>
2023-04-13 20:36:11 -0300

    modificaciones pylint

--- src/chatGPT/python_gpt.py
+++ src/chatGPT/python_gpt.py
@@ -1,59 +1,60 @@
-#!/usr/bin/python 
-import openai 
+"""este programa utiliza la API de OpenAI para crear un chatbot con el modelo GPT-3"""
 import argparse
-# configuración de argumentos de línea de comando
+import openai
+#configuración de argumentos de línea de comando
 parser = argparse.ArgumentParser(description='Programa de chat con GPT-3')
 parser.add_argument('--convers', action='store_true', help='Habilita el modo de conversación')
 args = parser.parse_args()
-# inicialización de la API de OpenAI
+#inicialización de la API de OpenAI
 openai.api_key = "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"
-# parámetros de la API
-TOP_P=1          
-FREQ_PENALTY=0          
-PRES_PENALTY=0          
-STOP=None 
-MAX_TOKENS=1024 
-TEMPERATURE=0.75 
-NMAX=1 
-MODEL_ENGINE = "text-davinci-003" 
-# buffer para almacenar las consultas y respuestas anteriores
+#parámetros de la API
+TOP_P=1
+FREQ_PENALTY=0
+PRES_PENALTY=0
+STOP=None
+MAX_TOKENS=1024
+TEMPERATURE=0.75
+NMAX=1
+MODEL_ENGINE = "text-davinci-003"
+#buffer para almacenar las consultas y respuestas anteriores
 conversation_buffer = []
-# función para imprimir el contenido de la consulta o la respuesta
+#función que imprimir el contenido de la consulta o la respuesta
 def print_message(message, is_user_message=True):
+    """Imprime el contenido de la consulta o la respuesta"""
     prefix = "You: " if is_user_message else "chatGPT: "
     print(prefix + message)
-# bucle principal para el modo de conversación
+#bucle principal para el modo de conversación
 while args.convers:
-    # aceptar consulta del usuario
+    #aceptar consulta del usuario
     user_input = input("Ingrese su consulta: ")
-    # si la consulta es nula, se ignora y se vuelve a pedir
+    #si la consulta es nula, se ignora y se vuelve a pedi
     if not user_input:
         continue
-    # salir del modo conversación al ingresar "salir"
+    #salir del modo conversación al ingresar "salir"
     if user_input == "salir":
         break
-    # agregar la consulta al buffer de conversación
+    #agregar la consulta al buffer de conversación
     conversation_buffer.append("You: " + user_input)
-    # combinar las consultas anteriores con la última para enviar a la API
-    prompt = "\n".join(conversation_buffer)
+    #combinar las consultas anteriores con la última para enviar a la API
+    PROMPT = "\n".join(conversation_buffer)
     try:
-        # invocar la API de chatGPT con la consulta actual y el buffer completo
+        #invocar la API de chatGPT con la consulta actual y el buffer completo
         completion = openai.Completion.create(
             engine=MODEL_ENGINE,
-            prompt=prompt,
+            prompt=PROMPT,
             max_tokens=MAX_TOKENS,
             n=NMAX,
             top_p=TOP_P,
             frequency_penalty=FREQ_PENALTY,
             presence_penalty=PRES_PENALTY,
             temperature=TEMPERATURE,
-            stop=["You:", "chatGPT:"] # indicar la cadena de STOP para el modo de conversación
+            stop=["You:", "chatGPT:"] #indicar la cadena de STOP para el modo de conversación
         )
-        # agregar la respuesta al buffer de conversación
+        #agregar la respuesta al buffer de conversación
         response = completion.choices[0].text.strip()
         conversation_buffer.append("chatGPT: " + response)
-        # imprimir la respuesta de chatGPT
+        #imprimir la respuesta de chatGPT
         print_message(response, False)
     except Exception as e:
-        # imprimir mensaje de error en caso de excepción
+        #imprimir mensaje de error en caso de excepción
         print("Se produjo un error:", e)
